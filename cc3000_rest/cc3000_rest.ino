eafe8610f28c8c5aba8b5081536793dcc63b97f4
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-03-27 09:52:15 +0100

    added printout of the CC3000 IP address

--- cc3000_rest/cc3000_rest.ino
+++ cc3000_rest/cc3000_rest.ino
@@ -63,6 +63,11 @@ void setup(void)
   }
   Serial.println();
   
+  // Print CC3000 IP address
+  while (! displayConnectionDetails()) {
+    delay(1000);
+  }
+  
   // Start multicast DNS responder
   if (!mdns.begin("arduino", cc3000)) {
     while(1); 
@@ -267,9 +272,32 @@ void loop() {
  
 }
 
+// Send HTTP headers
 void send_http_headers(Adafruit_CC3000_ClientRef client){
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
   client.println("Connection: close");
   client.println();
+}
+
+// Print connection details of the CC3000 chip
+bool displayConnectionDetails(void)
+{
+  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
+  
+  if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
+  {
+    Serial.println(F("Unable to retrieve the IP Address!\r\n"));
+    return false;
+  }
+  else
+  {
+    Serial.print(F("\nIP Addr: ")); cc3000.printIPdotsRev(ipAddress);
+    Serial.print(F("\nNetmask: ")); cc3000.printIPdotsRev(netmask);
+    Serial.print(F("\nGateway: ")); cc3000.printIPdotsRev(gateway);
+    Serial.print(F("\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
+    Serial.print(F("\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
+    Serial.println();
+    return true;
+  }
 }
